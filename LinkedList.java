9de11ec40d1bc2d8c2516885c6725cce0a02a111
keysie22 <keysie.malika@strathmore.edu>
2025-03-20 16:03:03 +0300

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,65 @@
+class LinkedList {
+    Node head; // Head (first node) of the linked list
+
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+        
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Insert at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Delete from the beginning
+    public void deleteFromBeginning() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;
+    }
+
+    // Display the list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(20);
+        list.insertAtEnd(30);
+        list.display(); // Output: 20 -> 10 -> 30 -> null
+
+        list.deleteFromBeginning();
+        list.display(); // Output: 10 -> 30 -> null
+    }
+}
